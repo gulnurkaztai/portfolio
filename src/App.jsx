ff3265006071a0cbbcd2d0938ac676e91d88637b
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-14 18:31:31 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from '../src/components/header/Header'
+import Projects from '../src/components/projects/Projects'
+import Contact from '../src/components/contact/Contact'
+import Footer from '../src/components/footer/Footer'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
