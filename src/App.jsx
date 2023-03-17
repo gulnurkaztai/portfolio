885bfab6b7e783b6e150581639a2319d912207a6
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-16 23:50:37 -0400

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from '../src/components/header/Header'
 import Projects from '../src/components/projects/Projects'
 import Contact from '../src/components/contact/Contact'
 import Footer from '../src/components/footer/Footer'
 
+
 function App() {
 
   return (
     <>
-      <Header/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-    </>
+    <Router>
+    <div className=''>
+
+
+      <Routes>
+      <Route path='/' element={<Header/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+
+      </Routes>
+
+    <Footer/>
+    </div>
+  </Router>
+
+  </>
+
+
+
+
   )
 }
 
