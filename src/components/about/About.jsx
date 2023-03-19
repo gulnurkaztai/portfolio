4910aa5c464b17658efdbb5c2bc7210568c14777
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-18 23:56:14 -0400

    added about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,22 @@
 import './about.css'
-
+import me from '../../assets/myImg.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={me} alt="" />
+          </div>
+        </div>
+        <div className='about__content'>
+          <p>Super-duper passionate</p>
+        </div>
+      </div>
+    </section>
   )
 }
 export default About
