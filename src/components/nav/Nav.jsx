92824be07d0de6a4fa236abcd8f45e89f6253d5c
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-18 23:57:02 -0400

    added navbar

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,21 @@
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {DiCodeBadge} from 'react-icons/di'
+import {FaRegUserCircle} from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {useState} from 'react'
+
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
+  
+
+  return (
+    <nav>
+        <a href='#' onClick={()=>setActiveNav('#')} className={activeNav=== '#'?'active':''}><AiOutlineHome/></a>
+        <a href='#projects' onClick={()=>setActiveNav('#projects')} className={activeNav=== '#projects'?'active':''}><DiCodeBadge/></a>
+        <a href='#about' onClick={()=>setActiveNav('#about')} className={activeNav=== '#about'?'active':''}><FaRegUserCircle/></a>
+        <a href='#contact' onClick={()=>setActiveNav('#contact')} className={activeNav=== '#contact'?'active':''}><HiOutlineMail/></a>
+    </nav>
+  )
+}
+export default Nav
