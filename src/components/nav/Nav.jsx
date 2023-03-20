6defd9dbddcb3039a4835efe4891ce2871d2044b
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-19 21:19:14 -0400

    styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {DiCodeBadge} from 'react-icons/di'
 import {FaRegUserCircle} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
+import {BsTools} from 'react-icons/bs'
 import {useState} from 'react'
 
 const Nav = () => {
@@ -14,6 +15,7 @@ const Nav = () => {
         <a href='#' onClick={()=>setActiveNav('#')} className={activeNav=== '#'?'active':''}><AiOutlineHome/></a>
         <a href='#projects' onClick={()=>setActiveNav('#projects')} className={activeNav=== '#projects'?'active':''}><DiCodeBadge/></a>
         <a href='#about' onClick={()=>setActiveNav('#about')} className={activeNav=== '#about'?'active':''}><FaRegUserCircle/></a>
+        <a href='#techstack' onClick={()=>setActiveNav('#techstack')} className={activeNav=== '#techstack'?'active':''}><BsTools/></a>
         <a href='#contact' onClick={()=>setActiveNav('#contact')} className={activeNav=== '#contact'?'active':''}><HiOutlineMail/></a>
     </nav>
   )
