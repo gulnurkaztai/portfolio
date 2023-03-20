7ca02219960790beb8262d86e76ddcd233a35f48
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-19 21:10:21 -0400

    projects page template

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,8 +1,29 @@
 import './projects.css'
+import thumbnail from '../../assets/pxfuel.jpg'
+import data from '../../data.json'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section id='projects'>
+      <h5>My Recent Work</h5>
+      <h2>Projects</h2>
+
+      <div className="container project__container">
+        {data.map((item)=>(
+          <article className="project__item">
+          <div className="project__item-image">
+            <img src={item.image} alt="" />
+          </div>
+          <h3>{item.title}</h3>
+          <p>{item.desc}</p>
+          <div className='project__item-cta'>
+            <a href={item.code} target="_blank" className='btn'>CODE</a>
+            <a href={item.live} target="_blank" className='btn btn-primary'>LIVE</a>
+          </div>
+        </article>
+        ))}
+      </div>
+    </section>
   )
 }
 export default Projects
