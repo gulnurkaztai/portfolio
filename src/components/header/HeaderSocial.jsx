38ec66b480fb836866a3b27ae10135d46d7b5a65
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-03-19 21:10:02 -0400

    styling

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -3,8 +3,8 @@ import {FaGithub, FaLinkedin} from 'react-icons/fa'
 const HeaderSocial = () => {
   return (
     <div className="header__socials">
-        <a to ='https://www.linkedin.com/in/gulnur-ospanova/'><FaLinkedin className="icon"/></a>
-        <a to ='https://github.com/gulnurkaztai' ><FaGithub className="icon"/></a>
+        <a href ='https://www.linkedin.com/in/gulnur-ospanova/'><FaLinkedin className="icon"/></a>
+        <a href ='https://github.com/gulnurkaztai' ><FaGithub className="icon"/></a>
     </div>
   )
 }
